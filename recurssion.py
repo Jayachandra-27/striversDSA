2f1d51138f8486b12e1ab5003b1afc4e39435eaf
Jayachandra-27 <jayachandrach2025@gmail.com>
2026-03-19 22:50:59 +0530

    Updated recursion function for sum of N numbers

--- recurssion.py
+++ recurssion.py
@@ -13,13 +13,36 @@
 # printnto1(10)
 
 # 3.Sum of first N Natural Numbers
-def sumofnnaturalnums(n):
-    if n>0:
-        if n==1:
-            return 1
-        else:
-            return n+sumofnnaturalnums(n-1)
-print(sumofnnaturalnums(7))
+# def sumofnnaturalnums(n):
+#     if n>0:
+#         if n==1:
+#             return 1
+#         else:
+#             return n+sumofnnaturalnums(n-1)
+# print(sumofnnaturalnums(7))
+
+# 4.Factorial of a Number : Iterative and Recursive
+# def factofnum(n):
+#     if n==0:
+#         return 1
+#     return n*factofnum(n-1)
+# print(factofnum(5))
+
+# 5.Reverse a given Array
+# def reverseAnArray(l):
+#     i=0
+#     j=len(l)-1
+#     while i<j:
+#         l[i],l[j]=l[j],l[i]
+#         i+=1
+#         j-=1
+#     return l
+# print(reverseAnArray([1,2,34,4,5,9]))
+
+# 6.Check if String is Palindrome or Not
+# def checkIfPalindrome(s,i,j):
+    
+
 
         
 
